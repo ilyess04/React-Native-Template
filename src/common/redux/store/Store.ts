36896baeec7ae17111c7c18b473fd731e21d7064
Sellamingo <ilyessellami04@gmail.com>
2024-07-04 16:37:14 +0100

    Async storage

--- src/common/redux/store/Store.ts
+++ src/common/redux/store/Store.ts
@@ -1,16 +1,43 @@
-import { combineReducers, configureStore } from "@reduxjs/toolkit";
-import { AuthReducer, SettingsReducer } from "../reducers";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import ReduxLogger from "redux-logger";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import {
+  persistReducer,
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "reduxjs-toolkit-persist";
+import { AuthReducer, SettingsReducer } from "../reducers";
 
-const reducers = combineReducers({
-  auth: AuthReducer,
-  settings: SettingsReducer,
-});
+
+const persistConfig = {
+  key: "root",
+  storage: AsyncStorage,
+  blackList: ["auth", "settings"],
+};
+
+const persistedReducer = persistReducer(
+  persistConfig,
+  combineReducers({
+    auth: AuthReducer,
+    settings: SettingsReducer,
+  })
+);
 
 const store = configureStore({
-  reducer: reducers,
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(ReduxLogger),
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }).concat(ReduxLogger),
 });
 
-export { store };
+const persistor = persistStore(store);
+
+export { store, persistor };
